a25ed69453a649a2647fb935c72de095520ecabf
Greds <161198516+gredss@users.noreply.github.com>
2025-04-17 15:57:10 +0700

    Update streamlit_hotel.py

--- streamlit_hotel.py
+++ streamlit_hotel.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 try:
-    model = joblib.load('XGB_trained_model.json')
+    model = joblib.load('XGB_trained_model.pkl')
 except Exception as e:
     st.error(f"Error loading model: {e}")
     st.stop()
